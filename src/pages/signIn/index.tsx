4a01a200131a43f39a43bf1f9972ba5fd48f4b46
이가빈 <leegabeen@igabin-ui-MacBookPro.local>
2023-06-06 16:05:27 +0900

    signin 카카오로그인 수정

--- src/pages/signIn/index.tsx
+++ src/pages/signIn/index.tsx
@@ -72,7 +72,7 @@ export default function SignInPage() {
   const onClickLoginKakao = ()  => {
     console.log('onClickLoginKakao')
     const REST_API_KEY = '7d3c474a3890053d011b65f24f038eb1';
-    const REDIRECT_URI =  `http://localhost:3000/sign-in?type=kakao`;
+    const REDIRECT_URI =  `http://cashbook-api-stage.jollybeen.com/sign-in?type=kakao`;
     
     window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`
   }
