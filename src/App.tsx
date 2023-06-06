53d2eaa3769f7d96791318557c2d1e83aaabd0a7
이가빈 <leegabeen@igabin-ui-MacBookPro.local>
2023-06-06 14:14:35 +0900

    로그인 페이지 오류 확인

--- src/App.tsx
+++ src/App.tsx
@@ -26,10 +26,20 @@ function App() {
     if(token === undefined) return
 
     if(token === null || token === '') {
+      console.log('token1', token)
+      console.log('locaion1', location.pathname)
       navigate('/sign-in')
     } else {
-      if(location.pathname === '/' || location.pathname === '/sign-in') navigate('/')
-      else navigate(`${location.pathname}`)
+      if(location.pathname === '/' || location.pathname === '/sign-in') {
+        console.log('token2', token)
+        console.log('locaion2', location.pathname)
+        navigate('/')
+      }
+      else {
+        console.log('token3', token)
+        console.log('locaion3', location.pathname)
+        navigate(`${location.pathname}`)
+      }
     }
 
   }, [tokenRef.current])
